d6b713abc1cccd727002d92c00cc59ca13447e4a
Łukasz Gorzelak <lukaszgorzelak91@gmail.com>
2024-02-08 11:23:32 +0100

    deploy project

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { defineProps } from 'vue';
+    
+    const props = defineProps({
+        className: {
+            type: String,
+            default:'test',
+        }
+    })
+</script>
+
+<template>
+    <button :class="className">
+        <slot></slot>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+    button {
+        padding: 8px 16px;
+        border: none;
+    }
+</style>
